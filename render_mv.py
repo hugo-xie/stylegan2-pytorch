333a3838261f859a896bde920909761de400a9c6
hugo-xie <xieyu9332@gmail.com>
2020-11-29 18:05:28 +0800

    toonify

--- render_mv.py
+++ render_mv.py
@@ -4,6 +4,11 @@ import os
 resize = 256
 
 img_paths = glob(os.path.join("./sample", "*.png"))
+
 for img in img_paths:
+    flag = True
     command = "python gennerate_f_image.py  --ckpt stylegan2-ffhq-config-f.pt --size 1024  {:s}".format(img)
-    tem = os.popen(command)
+    while flag:
+        file_name = os.path.splitext(os.path.basename(img))[0] + "-project.png"
+        if os.path.exists(file_name):
+            flag = False
